b7fb9699610551989f6a6112dc1dce42a4177431
springhana <xksxksanfro7@naver.com>
2024-06-08 15:43:44 +0900

    ⚙️setting:  이미지 세팅

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["dodo.ac", "acnhcdn.com"],
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+  env: {
+    BASE_URL: process.env.BASE_URL,
+    X_API_KEY: process.env.X_API_KEY,
+  },
+};
 
 export default nextConfig;
