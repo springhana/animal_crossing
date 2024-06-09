b7bad72cd884697d1f9aa11b608e6e36ea93b889
springhana <xksxksanfro7@naver.com>
2024-06-09 12:29:00 +0900

    ✨feat:  스켈레톤 UI 추가

---
+++ src/components/skeleton/SkeletonItem.tsx
@@ -0,0 +1,5 @@
+export default function SkeletonItem() {
+  return (
+    <div className="skeleton-list-item bg-[#ECECEC] w-[200px] h-[200px] m-auto rounded-full"></div>
+  );
+}
---
+++ src/components/skeleton/SkeletonSeeMore.tsx
@@ -0,0 +1,5 @@
+export default function SkeletonSeeMore() {
+  return (
+    <div className="skeleton-list-item bg-[#ECECEC] w-[100px] h-[100px] m-auto rounded-full"></div>
+  );
+}
