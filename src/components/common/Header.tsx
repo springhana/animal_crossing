d56bd590e9da6d6a02109c2a66feeb26bd4e15f3
springhana <xksxksanfro7@naver.com>
2024-06-10 15:20:50 +0900

    🔨modify:  제목에 라우터 추가

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,14 +1,21 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
+
 import EmptyIcon from '@/assets/icon/empty-item.svg';
 import useMenuModalStore from '@/store/useMenuModalStore';
 
 export default function Header() {
   const { isOpen, onOpen, onClose } = useMenuModalStore();
+  const router = useRouter();
 
   const handleOnClickMenu = () => {
     if (isOpen) onClose();
     else onOpen();
+  };
+
+  const handleOnClickHomeRouter = () => {
+    router.push('/');
   };
 
   return (
@@ -16,7 +23,9 @@ export default function Header() {
       className={`w-full min-w-min_w flex justify-between items-center ${
         !isOpen && 'max-w-max_w'
       } h-[6.25rem] m-auto p-2 mobile:w-screen`}>
-      <div className="w-full text-center">
+      <div
+        className="w-full text-center cursor-pointer"
+        onClick={handleOnClickHomeRouter}>
         <h1 className="text-bs_43 mobile:text-bs_28 flex justify-center mobile:justify-start items-center gap-2">
           <EmptyIcon className="mobile:scale-75" /> 모동숲 백과사전
         </h1>
