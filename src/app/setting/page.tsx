b73c727a4b5db4f9aba7e59fcd57453ed009ed2e
springhana <xksxksanfro7@naver.com>
2024-06-10 15:18:08 +0900

    ✨feat:  세팅 및 정보 페이지 추가

--- src/app/setting/page.tsx
+++ src/app/setting/page.tsx
@@ -1,3 +1,27 @@
+'use client';
+
+import GithubIcon from '@/assets/icon/github.svg';
+import { CompoundList } from '@/components/common/CompoundList';
+
 export default function Setting() {
-  return <div>page</div>;
+  return (
+    <CompoundList>
+      <h1 className="text-bs_34 my-10">
+        Nookipedia에서 제공해준 모여봐요 동물의숲 오픈 API를 사용하여 만든
+        모동숲 백과사전 사이트입니다.
+      </h1>
+
+      <div className="flex items-center">
+        <GithubIcon width="60" height="60" />
+        <a href="https://api.nookipedia.com/doc">Nookipedia 오픈 API</a>
+      </div>
+
+      <div className="flex items-center">
+        <GithubIcon width="60" height="60" />
+        <a href="https://github.com/springhana/animal_crossing">
+          https://github.com/springhana/animal_crossing
+        </a>
+      </div>
+    </CompoundList>
+  );
 }
